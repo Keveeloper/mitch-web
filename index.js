7e5aa62440e7fcc9bc88a6f3d7efa2f5f78d5b9f
MitchellArevalo <107509488+MitchellArevalo@users.noreply.github.com>
2022-09-28 17:21:19 -0500

    cambios animaciones

---
+++ index.js
@@ -0,0 +1,40 @@
+window.addEventListener("scroll",function(){
+    var aboutUs = document.getElementById("aboutUs");
+    var positionAboutUs = aboutUs.getBoundingClientRect().top;
+    var tamañoPantalla = window.innerHeight;
+    console.log(positionAboutUs);
+
+    if(positionAboutUs != 0){
+        this.document.querySelector("header").style.position = 'fixed';
+        this.document.querySelector("header").style.backgroundColor = 'black';
+    }
+    else{
+        this.document.querySelector("header").style.position = 'relative';
+        this.document.querySelector("header").style.backgroundColor = 'transparent';
+    }
+   
+    var cardsBackground = document.getElementById("about-us-container");
+    var positionCards = cardsBackground.getBoundingClientRect().top;
+    var x1 = (positionCards /3);
+    if(positionCards < tamañoPantalla){
+        cardsBackground.style.transform = "translate(0px," + x1 + "px)";
+    }
+
+    var divMitchell = document.getElementById("card-Mitchell");
+    var positionMitchell = divMitchell.getBoundingClientRect().top;
+    var tamañoPantalla = window.innerHeight;
+    
+    var x1 = (positionMitchell * -1) + 150;
+   
+    if(positionMitchell < tamañoPantalla){
+        divMitchell.style.transform = "translate(" + x1 + "px,0px)";
+    }
+
+    var divKevind = document.getElementById("card-Kevind");
+    var positionKevind = divKevind.getBoundingClientRect().top;
+    
+    var x = positionKevind - 150;
+    if(positionKevind < tamañoPantalla){
+        divKevind.style.transform = "translate(" + x + "px,0px)";
+    }
+});
